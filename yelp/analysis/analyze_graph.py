828fea3578def1babd61b7807e3bb2060f09ac5d
yan cui <yan.cui@hulu.com>
2015-11-23 13:20:02 -0800

    yelp user analysis and number of components

--- yelp/analysis/analyze_graph.py
+++ yelp/analysis/analyze_graph.py
@@ -8,9 +8,6 @@ __author__ = 'yan.cui'
 
 # percentage against neighbor's degree
 
-# when fans number is larger, compliments activity and votes should be more active
-# which mechanism is more effective? votes or compliments?
-
 # which one is more effective? tip or review?
 
 # how location affect users on yelp, we have location of business, how many uses cross
@@ -61,18 +58,19 @@ def stars_distribution():
     pass
 
 def component_numbers():
-    # match (n:User)
+    # Users with no friends 192621
+    # match (n:User) where (n)-[:knows]-()
     # with collect(n) as nodes
     # with reduce(graphs = [], n in nodes |
      #  case when
      #  ANY (g in graphs where shortestPath( (n)-[*]-(g) ) )
      #  then graphs
      #  else graphs + [n]
-     #  end ) as result unwind result as ret return count(ret)
-     # this is super slow
+     #  end ) as result unwind result as ret return count(ret)  2434 components
     pass
 
 def component_distribution():
+
     pass
 
 def get_random_nodeid():
@@ -88,7 +86,7 @@ def hop_distance_distribution():
     # shortest path between two nodes
     # match(m:Test),(n:Test) where id(m)=39609 and id(n)=39608
     # with shortestPath((m)-[*]-(n)) as path return length(path)
-    experiment, inf, MAX_ITERARIONS = 0, 32767, 500
+    experiment, inf, MAX_ITERARIONS = 0, 32767, 10000
     dist = {}
     for index in range(MAX_ITERARIONS):
         id1, id2 = get_random_nodeid(), get_random_nodeid()
@@ -132,8 +130,73 @@ def fans_distribution():
 def elite_distribution():
     # match(n:User) where n.elite=[] return count(n)  341414
     # most are not elite users
-    # match(n:User) where n.elite<>[] return avg(n.fans)
+    # match(n:User) where n.elite<>[] return avg(n.fans) = 16.3822773803407 ~= 16
+    # match(n:User) where n.elite<>[] return avg(n.review_count) = 245.09220979407928 ~= 245
+    # match(n:User) where n.elite<>[] return avg(n.avg_stars) = 3.7801367534879953
+    # match(n:User) where n.elite<>[] return avg(toInt(n.votes[1]) + toInt(n.votes[3]) + toInt(n.votes[5])) = 1336.1507845539702
+    # match(n:User) where n.elite<>[] return count(n) 25301
+    # match(n:User) where n.elite<>[] and n.compliments<>[] return count(n) 25244
+    # match(n:User) where n.elite<>[] and n.yelping_since =~ "2004-.*" return count(n)  22
+    # 2004 22    / 51       =   0.43137254901960786
+    # 2005 357   / 691      =   0.516642547033285
+    # 2006 1525  / 3974     =   0.3837443381982889
+    # 2007 3120  / 10676    =   0.29224428624953164
+    # 2008 4198  / 19390    =   0.21650335224342446
+    # 2009 4631  / 32968    =   0.140469546226644
+    # 2010 4649  / 50722    =   0.09165648042269627
+    # 2011 3571  / 69210    =   0.05159659008813755
+    # 2012 1926  / 63897    =   0.03014226020000939
+    # 2013 977   / 63483    =   0.015389946914922105
+    # 2014 325   / 50505    =   0.006435006435006435
+    # 2015 0     / 1148
+    # elite is effective
     pass
+
+def review_count_distribution():
+    # match(n:User) where n.review_count=0 return count(n) 7
+    # match(n:User) where n.review_count > 0 and review_count <= 100 return count(n) 338773
+    # match(n:User) where n.review_count > 100 and review_count <= 200 return count(n) 15094
+    # match(n:User) where n.review_count > 200 and review_count <= 300 return count(n) 5846
+    # match(n:User) where n.review_count > 300 and review_count <= 400 return count(n) 2818
+    # match(n:User) where n.review_count > 400 and review_count <= 500 return count(n) 1563
+    # match(n:User) where n.review_count > 500 return count(n) 2614
+    pass
+
+def votes_distribution():
+    # match(n:User) where toInt(n.votes[1]) + toInt(n.votes[3]) + toInt(n.votes[5]) = 0 return count(n) 56420
+    # match(n:User) return  max(toInt(n.votes[1]) + toInt(n.votes[3]) + toInt(n.votes[5])) 100319
+    # match(n:User) where toInt(n.votes[1]) + toInt(n.votes[3]) + toInt(n.votes[5]) > 0 and toInt(n.votes[1]) +
+    # toInt(n.votes[3]) + toInt(n.votes[5]) <= 1000 return count(n) 302842
+    # > 1000 and <=2000 3842
+    # > 2000 and <= 3000 1350
+    # > 3000 and <= 4000 665
+    # > 4000 and <= 5000  410
+    # > 5000 1186
+    pass
+
+def compliments_distribution():
+    # match(n:User) where n.compliments=[] return count(n)  223476  most users will not write compliments
+    # match(n:User) where n.votes[1]="0" and n.votes[3] = "0" and n.votes[5] = "0" return count(n) 56420 which
+    # is much smaller than the number of users do not write compliments, indicates most people just want to
+    # write a simple votes instead of compliments(compliments show more details of the comments)
+    pass
+
+def user_increase_ratio():
+    # analyze the yelp-since field
+    # match(n:User) where n.yelping_since =~ '2004-.*' return count(n) 51
+    # match(n:User) where n.yelping_since =~ '2005-.*' return count(n) 691
+    # "2006" 3974
+    # "2007" 10676
+    # "2008" 19390
+    # "2009" 32968
+    # "2010" 50722
+    # "2011" 69210
+    # "2012" 63897
+    # "2013" 63483
+    # "2014" 50505
+    # "2015" 1148
+    pass
+
 if __name__ == "__main__":
     hop_distance_distribution()
 
